3a5e554811421fc07e2607299a6b70cc80666436
cmoulliard <cmoulliard@redhat.com>
2025-01-30 10:18:09 +0100

    Initial upload
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

---
+++ src/test/java/org/acme/KindClusterTest.java
@@ -0,0 +1,24 @@
+package org.acme;
+import com.dajudge.kindcontainer.KindContainer;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static io.fabric8.kubernetes.client.Config.fromKubeconfig;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Testcontainers
+public class KindClusterTest {
+    @Container
+    public static final KindContainer<?> KUBE = new KindContainer<>();
+
+    @Test
+    public void verify_node_is_present() {
+        // Create a fabric8 client and use it!
+        try (final KubernetesClient client = new DefaultKubernetesClient(fromKubeconfig(KUBE.getKubeconfig()))) {
+            assertEquals(1, client.nodes().list().getItems().size());
+        }
+    }
+}
