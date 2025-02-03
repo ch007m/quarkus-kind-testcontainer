d126e7557f814941849a29a1c680093fe740fa0c
cmoulliard <cmoulliard@redhat.com>
2025-02-03 09:29:03 +0100

    Test is passing but we added a Thread sleep of 2min
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- src/test/java/org/acme/ArgoCDDeployTest.java
+++ src/test/java/org/acme/ArgoCDDeployTest.java
@@ -7,9 +7,7 @@ import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.dsl.Resource;
 import io.quarkiverse.argocd.v1alpha1.Application;
-import io.quarkus.logging.Log;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +15,6 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import static io.fabric8.kubernetes.client.Config.fromKubeconfig;
@@ -45,10 +42,16 @@ public class ArgoCDDeployTest {
     private static final String ARGOCD_DEPLOYMENT_DEX_SERVER_NAME = "argocd-dex-server";
     private static final String ARGOCD_DEPLOYMENT_NOTIFICATION_CONTROLLER_NAME = "argocd-notifications-controller";
     private static final String ARGOCD_DEPLOYMENT_APPLICATIONSET_CONTROLLER_NAME = "argocd-applicationset-controller";
+
     private static final String ARGOCD_POD_APP_CONTROLLER_NAME = "argocd-application-controller-0";
+    private static final String ARGOCD_POD_APPLICATIONSET_CONTROLLER_NAME = "argocd-applicationset-controller";
+    private static final String ARGOCD_POD_SERVER_NAME = "argocd-server";
+    private static final String ARGOCD_POD_REDIS_NAME = "argocd-redis";
+    private static final String ARGOCD_POD_REPO_SERVER_NAME = "argocd-repo-server";
+    private static final String ARGOCD_POD_DEX_SERVER_NAME = "argocd-dex-server";
+    private static final String ARGOCD_POD_NOTIFICATION_CONTROLLER_NAME = "argocd-notifications-controller";
 
     private static final String DEPLOYMENT_STATUS_AVAILABLE = "Available";
-    private static final String POD_STATUS_AVAILABLE = "Ready";
 
     final KubernetesClient client = new DefaultKubernetesClient(fromKubeconfig(KUBE.getKubeconfig()));
 
@@ -70,7 +73,7 @@ public class ArgoCDDeployTest {
         client.resources(Pod.class)
             .inNamespace(ns)
             .withName(name)
-            .waitUntilReady(30, TimeUnit.SECONDS);
+            .waitUntilReady(180, TimeUnit.SECONDS);
         LOG.info("Pod: {} ready in {}", name, ns);
     }
 
@@ -78,7 +81,7 @@ public class ArgoCDDeployTest {
         client.resources(Pod.class)
             .inNamespace(ns)
             .withLabel(key, value)
-            .waitUntilReady(30, TimeUnit.SECONDS);
+            .waitUntilReady(180, TimeUnit.SECONDS);
         LOG.info("Pod: {} ready in {}", value, ns);
     }
 
@@ -117,16 +120,21 @@ public class ArgoCDDeployTest {
         });
 
 
-        checkDeploymentReady(ARGOCD_DEPLOYMENT_SERVER_NAME);
+/*      checkDeploymentReady(ARGOCD_DEPLOYMENT_SERVER_NAME);
         checkDeploymentReady(ARGOCD_DEPLOYMENT_REDIS_NAME);
         checkDeploymentReady(ARGOCD_DEPLOYMENT_REPO_SERVER_NAME);
         checkDeploymentReady(ARGOCD_DEPLOYMENT_DEX_SERVER_NAME);
         checkDeploymentReady(ARGOCD_DEPLOYMENT_NOTIFICATION_CONTROLLER_NAME);
-        checkDeploymentReady(ARGOCD_DEPLOYMENT_APPLICATIONSET_CONTROLLER_NAME);
+        checkDeploymentReady(ARGOCD_DEPLOYMENT_APPLICATIONSET_CONTROLLER_NAME);*/
 
         // Waiting till the pods are ready/running ...
+        waitTillPodByLabelReady(ARGOCD_NS,"app.kubernetes.io/name",ARGOCD_POD_REDIS_NAME);
+        waitTillPodByLabelReady(ARGOCD_NS,"app.kubernetes.io/name",ARGOCD_POD_REPO_SERVER_NAME);
+        waitTillPodByLabelReady(ARGOCD_NS,"app.kubernetes.io/name",ARGOCD_POD_DEX_SERVER_NAME);
+        waitTillPodByLabelReady(ARGOCD_NS,"app.kubernetes.io/name",ARGOCD_POD_SERVER_NAME);
+        waitTillPodByLabelReady(ARGOCD_NS,"app.kubernetes.io/name",ARGOCD_POD_NOTIFICATION_CONTROLLER_NAME);
+        waitTillPodByLabelReady(ARGOCD_NS,"app.kubernetes.io/name",ARGOCD_POD_APPLICATIONSET_CONTROLLER_NAME);
         waitTillPodReady(ARGOCD_NS, ARGOCD_POD_APP_CONTROLLER_NAME);
-        waitTillPodByLabelReady(ARGOCD_NS,"app.kubernetes.io/name","argocd-server");
 
         // Populate the Argocd resources
         Config config = new Config();
@@ -139,6 +147,14 @@ public class ArgoCDDeployTest {
             .inNamespace(ARGOCD_NS)
             .create();
 
+        // Sleep 2 minutes
+        try {
+            Thread.sleep(120000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        LOG.info("Checking when Argocd Application will be Healthy");
         client.resources(Application.class)
             .inNamespace(ARGOCD_NS)
             .withName(config.getApplicationName())
@@ -146,32 +162,33 @@ public class ArgoCDDeployTest {
                 a != null &&
                 a.getStatus() != null &&
                 a.getStatus().getHealth() != null &&
-                a.getStatus().getHealth().getStatus().equals("Healthy") &&
-                a.getStatus().getSync().getStatus().equals("Synced"),1800, TimeUnit.SECONDS);
+                a.getStatus().getHealth().getStatus() != null &&
+                a.getStatus().getHealth().getStatus().equals("Healthy"),3600, TimeUnit.SECONDS);
+        LOG.info("Argocd Application: {} healthy", config.getApplicationName());
 
-        // TODO: Investigate why the pod is never created as status of Sync never become => synced
-        // or if error is reported by argocd server/controller
-        // journalctl -xeu kubelet
-        // alias k=kubectl
+        /*
+         journalctl -xeu kubelet
+         alias k=kubectl
+         k get po -A
+         k get applications -A
+         k get AppProject -A
+        */
 
-        // Argocd Server
-        // time="2025-01-31T13:16:27Z" level=warning msg="Failed to resync revoked tokens. retrying again in 1 minute: dial tcp 10.245.14.250:6379: connect: connection refused"
-
-        // Application Controller
-        // time="2025-01-31T13:16:23Z" level=info msg="Normalized app spec: {\"status\":{\"conditions\":[{\"lastTransitionTime\":\"2025-01-31T13:16:23Z\",\"message\":\"Failed to load target state: failed to generate manifest for source 1 of 1: rpc error: code = Unavailable desc = connection error: desc = \\\"transport: Error while dialing: dial tcp 10.245.254.103:8081: connect: connection refused\\\"\",\"type\":\"ComparisonError\"}]}}" app-namespace=argocd app-qualified-name=argocd/test-1 application=test-1 project=default
-        // time="2025-01-31T13:16:26Z" level=error msg="Failed to cache app resources: error setting app resource tree: dial tcp 10.245.14.250:6379: connect: connection refused" app-namespace=argocd app-qualified-name=argocd/test-1 application=test-1 comparison-level=3 dedup_ms=0 dest-name= dest-namespace=argocd dest-server="https://kubernetes.default.svc" diff_ms=0 git_ms=360 health_ms=0 live_ms=0 project=default settings_ms=0 sync_ms=0
-        // time="2025-01-31T13:16:26Z" level=info msg="Skipping auto-sync: application status is Unknown" app-namespace=argocd app-qualified-name=argocd/test-1 application=test-1 project=default
-        // time="2025-01-31T13:16:26Z" level=info msg="Updated sync status:  -> Unknown" application=test-1 dest-namespace=argocd dest-server="https://kubernetes.default.svc" reason=ResourceUpdated type=Normal
-        // time="2025-01-31T13:16:26Z" level=info msg="Updated health status:  -> Healthy" application=test-1 dest-namespace=argocd dest-server="https://kubernetes.default.svc" reason=ResourceUpdated type=Normal
-        // time="2025-01-31T13:16:26Z" level=info msg="Update successful" app-namespace=argocd app-qualified-name=argocd/test-1 application=test-1 project=default
-        // time="2025-01-31T13:16:26Z" level=info msg="Reconciliation completed" app-namespace=argocd app-qualified-name=argocd/test-1 app_status_update_ms=0 application=test-1 auto_sync_ms=0 compare_app_state_ms=360 comparison-level=3 comparison_with_nothing_ms=0 dedup_ms=0 dest-name= dest-namespace=argocd dest-server="https://kubernetes.default.svc" diff_ms=0 git_ms=360 health_ms=0 live_ms=0 normalize_application_ms=2 patch_ms=3 persist_app_status_ms=7 process_finalizers_ms=0 project=default refresh_app_conditions_ms=0 set_app_managed_resources_ms=3156 setop_ms=0 settings_ms=0 sync_ms=0 time_ms=3527
-
+        LOG.info("Checking now when Argocd Application will be synced");
+        client.resources(Application.class)
+            .inNamespace(ARGOCD_NS)
+            .withName(config.getApplicationName())
+            .waitUntilCondition(a ->
+                a != null &&
+                a.getStatus() != null &&
+                a.getStatus().getSync() != null &&
+                a.getStatus().getSync().getStatus() != null &&
+                a.getStatus().getSync().getStatus().equals("Synced"), 3600, TimeUnit.SECONDS);
+        LOG.info("Argocd Application: {} synced", config.getApplicationName());
 
         Application app = client.resources(Application.class)
             .inNamespace(ARGOCD_NS)
             .withName(config.getApplicationName()).get();
-
-        LOG.info("Argocd Application is ready: {}", config.getApplicationName());
         LOG.info(client.getKubernetesSerialization().asYaml(app));
     }
 }
